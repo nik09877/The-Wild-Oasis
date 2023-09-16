c830a6c1bd4b35171d2bcd21d0fe5e16a51775ac
Nikhil Kumar Mishra <nikhilkumarmishra120@gmail.com>
2023-09-17 01:00:21 +0530

    finished the show all room-listings part

--- app/components/inputs/input.tsx
+++ app/components/inputs/input.tsx
@@ -2,6 +2,7 @@
 
 import { FieldErrors, FieldValues, UseFormRegister } from 'react-hook-form';
 import { BiDollar } from 'react-icons/bi';
+import { PiCurrencyInr } from 'react-icons/pi';
 
 interface InputProps {
   id: string;
@@ -27,7 +28,7 @@ const Input: React.FC<InputProps> = ({
   return (
     <div className='w-full relative'>
       {formatPrice && (
-        <BiDollar
+        <PiCurrencyInr
           size={24}
           className='
             text-neutral-700
