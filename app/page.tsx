6f0d3ca4693ebc11bad7e9516ee699ad6ce730f4
Nikhil Kumar Mishra <nikhilkumarmishra120@gmail.com>
2023-09-21 11:46:11 +0530

    changed Home page to client component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import getCurrentUser from '@/app/actions/getCurrentUser';
 import ClientOnly from './components/ClientOnly';
 import HomePageClient from './HomePageClient';
 
+//SERVER COMPONENT
 const Home = async () => {
   const currentUser = await getCurrentUser();
 
